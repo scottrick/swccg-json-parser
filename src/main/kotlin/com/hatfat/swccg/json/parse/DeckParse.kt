2bd4849b9137d2536f10544c487bd7b5716f0b3f
Scott Atkins <scottrick49@gmail.com>
2024-03-16 21:53:17 -0500

    Work on parsing DechTech decks.

--- src/main/kotlin/com/hatfat/swccg/json/parse/DeckParse.kt
+++ src/main/kotlin/com/hatfat/swccg/json/parse/DeckParse.kt
@@ -1,17 +1,24 @@
 package com.hatfat.swccg.json.parse
 
+import com.hatfat.swccg.json.parse.data.SWCCGCard
 import com.hatfat.swccg.json.parse.data.SWCCGDeck
 import java.io.File
 
 class DeckParse(
-    private val directory: String
+    private val directory: String,
+    private val allCards: List<SWCCGCard>
 ) {
+    private val processedCards = processCardNames(allCards)
+    private val cardNames = processedCards.keys
+
     fun parse() {
-        println("Decktech Parse")
+        println("DeckTech Parse")
+        println("${allCards.size} cards loaded")
+
         var count = 0
 
         val dir = File(directory).walkTopDown().forEach { file ->
-            if (file.extension == "md" && count < 4000) {
+            if (file.extension == "md" && count < 4) {
 //                println("found deck: " + file.name)
                 parseFile(file)
                 count++
@@ -21,7 +28,7 @@ class DeckParse(
         println("Parsed $count decks.")
     }
 
-    fun parseFile(deckFile: File) {
+    private fun parseFile(deckFile: File) {
 //        ---
 //        layout: deck
 //        author: ! Mike "Iceman" Fitzgerald
@@ -62,18 +69,87 @@ class DeckParse(
         val strategyIndex = lines.indexOf(strategyLine)
         assert(strategyIndex != -1)
 
-        parseCards(deck, lines)
+        val cardLines = lines.subList(0, strategyIndex)
 
+        parseCards(deck, cardLines)
     }
 
-    fun parseHeader(deck: SWCCGDeck, lines: List<String>) {
+    private fun parseHeader(deck: SWCCGDeck, lines: List<String>) {
 //        println(lines)
 
     }
 
-    fun parseCards(deck: SWCCGDeck, lines: List<String>) {
-        assert(lines[0].startsWith("Cards:"))
-        assert(lines[1] == "")
+    private fun parseCards(deck: SWCCGDeck, rawLines: List<String>) {
+        assert(rawLines[0].startsWith("Cards:"))
+        assert(rawLines[1] == "")
+        val lines = rawLines.drop(2)
         println(lines)
+
+//        val headerRegex = "/\\w+/gm".toRegex()
+        val headerRegex = """\w+""".toRegex()
+        val reg = Regex("""[0-9]*""")
+
+        val cards = mutableListOf<String>()
+
+        for (line in lines) {
+            if (line.trim().isBlank() || line.trim().isEmpty() || line == "'") {
+                continue
+            }
+//            println("checking line: $line")
+
+//            val words = headerRegex.findAll(line).toList(c)
+//            if (words.isNotEmpty() && words[0].value.trim().startsWith("start", true)) {
+//                println("found STARTING CARDS! $line")
+//            }
+
+            // First try to find an exact match
+            val exact = cardNames.filter { cardName ->
+                cardName.equals(line, true)
+            }
+
+            if (exact.size == 1) {
+                // found one card that matches, add it to the deck
+                cards.add(exact[0])
+                continue
+            }
+
+            var filtered = partialContains(line)
+            if (filtered.isNotEmpty()) {
+                println("line [$line] filtered ${filtered.size}, matches: $filtered")
+            }
+//            println("Found no match: $line")
+        }
+
+        println("FINISHED parsing deck [${cards.size}]:")
+        for (card in cards) {
+            println("  $card")
+        }
     }
+
+    fun partialContains(line: String): List<String> {
+        return cardNames.filter { cardName ->
+            cardName.contains(line, true) || line.contains(cardName, true)
+        }
+    }
+}
+
+fun processCardNames(cards: List<SWCCGCard>): HashMap<String, SWCCGCard> {
+    val processedCardNames = HashMap<String, SWCCGCard>()
+
+    for (card in cards) {
+        card.front.title?.let { title ->
+            var processedTitle = title
+            // String starting •
+            while (processedTitle.indexOf("•", 0, true) != -1) {
+                val index = processedTitle.indexOf("•", 0, true)
+                processedTitle = processedTitle.removeRange(index, index + 1)
+//                processedTitle = processedTitle.drop(1)
+            }
+
+//            println("$title -> $processedTitle")
+            processedCardNames[processedTitle] = card
+        }
+    }
+
+    return processedCardNames
 }
--- src/main/kotlin/com/hatfat/swccg/json/parse/Main.kt
+++ src/main/kotlin/com/hatfat/swccg/json/parse/Main.kt
@@ -1,20 +1,55 @@
 package com.hatfat.swccg.json.parse
 
+import com.google.gson.Gson
 import com.google.gson.GsonBuilder
+import com.hatfat.swccg.json.parse.data.SWCCGCard
+import com.hatfat.swccg.json.parse.data.SWCCGCardList
+import java.io.BufferedReader
+import java.io.File
+import java.io.FileInputStream
+import java.io.InputStreamReader
 
 @Suppress("UNUSED_PARAMETER")
 fun main(args: Array<String>) {
-//    val gson = GsonBuilder()
-//        .setPrettyPrinting()
-//        .disableHtmlEscaping()
-//        .create()
-//
+    val gson = GsonBuilder()
+        .setPrettyPrinting()
+        .disableHtmlEscaping()
+        .create()
+
 //    val cardParse = CardParse(gson)
 //    cardParse.lightDarkJsonWork()
 
 //    parseSets(gson)
 
-    val deckParse = DeckParse("../decktech.net/_decks")
+    val cards = getAllCards(gson)
+
+    val deckParse = DeckParse("../decktech.net/_decks", cards)
     deckParse.parse()
+}
+
+fun getAllCards(gson: Gson): List<SWCCGCard> {
+    val lightCurrentInputStream = FileInputStream(File("output/Light.json"))
+    val lightCurrentReader = BufferedReader(InputStreamReader(lightCurrentInputStream))
+    val light = gson.fromJson(lightCurrentReader, SWCCGCardList::class.java)
+
+    val darkCurrentInputStream = FileInputStream(File("output/Dark.json"))
+    val darkCurrentReader = BufferedReader(InputStreamReader(darkCurrentInputStream))
+    val dark = gson.fromJson(darkCurrentReader, SWCCGCardList::class.java)
+
+    val lightLegacyInputStream = FileInputStream(File("output/LightLegacy.json"))
+    val lightLegacyReader = BufferedReader(InputStreamReader(lightLegacyInputStream))
+    val lightLegacy = gson.fromJson(lightLegacyReader, SWCCGCardList::class.java)
+
+    val darkLegacyInputStream = FileInputStream(File("output/DarkLegacy.json"))
+    val darkLegacyReader = BufferedReader(InputStreamReader(darkLegacyInputStream))
+    val darkLegacy = gson.fromJson(darkLegacyReader, SWCCGCardList::class.java)
+
+    val cards = mutableListOf<SWCCGCard>()
+    cards.addAll(light.cards)
+    cards.addAll(dark.cards)
+    cards.addAll(lightLegacy.cards)
+    cards.addAll(darkLegacy.cards)
+
+    return cards
 }
 
